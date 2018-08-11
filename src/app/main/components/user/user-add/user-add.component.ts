6e7f3978d47d6369b8256024b9784c0276608df4
bimaaghafara <bimaa7@gmail.com>
2018-08-12 02:10:48 +0700

    onProgress addUser

--- src/app/main/components/user/user-add/user-add.component.ts
+++ src/app/main/components/user/user-add/user-add.component.ts
@@ -10,6 +10,15 @@ import { LoginService } from '../../../services/login.service';
   styleUrls: ['./user-add.component.scss']
 })
 export class UserAddComponent implements OnInit {
+  model = {
+    userName: '', firstName: '', lastName: '', active: false, password: '', confirmPassword: ''
+  };
+
+  isValid = {
+    userName: true, firstName: true, lastName: true, password: true, confirmPassword: true
+  };
+
+  isAllowSubmit = false;
 
   constructor(
     private loginService: LoginService,
@@ -17,6 +26,26 @@ export class UserAddComponent implements OnInit {
 
   ngOnInit() {
     this.loginService.isLogin();
+  }
+
+  doValidation() {
+    this.isAllowSubmit = true;
+    const modelKeys = ['userName', 'firstName', 'lastName', 'password', 'confirmPassword'];
+    modelKeys.map(modelKey => {
+      this.isValid[modelKey] = this.model[modelKey] ? true : false;
+      this.isAllowSubmit = this.isAllowSubmit && this.model[modelKey];
+    });
+    this.isAllowSubmit = this.isAllowSubmit ? true : false;
+  }
+
+  onSubmit() {
+    this.doValidation();
+    console.log('Button add user was clicked!');
+    console.log(this.isAllowSubmit);
+  }
+
+  onCancel() {
+    console.log(this.model);
   }
 
 }
