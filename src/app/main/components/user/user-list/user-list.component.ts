911ca62e2d0d71c450684043bc26055e53d9d88c
bimaaghafara <bimaa7@gmail.com>
2018-08-11 10:25:45 +0700

    add checkbox, btn edit, btn add, btn delete

--- src/app/main/components/user/user-list/user-list.component.ts
+++ src/app/main/components/user/user-list/user-list.component.ts
@@ -12,7 +12,16 @@ import { UserService } from '../../../services/user.service';
 export class UserListComponent implements OnInit {
 
   users;
-  columns = ['userName', 'firstName', 'lastName'];
+  columns = [
+    {name: 'userName', label: 'Username'},
+    {name: 'firstName', label: 'First Name'},
+    {name: 'lastName', label: 'Last Name'},
+    {name: 'active', label: 'Active'},
+    {name: 'lastModifiedBy', label: 'Last Modified By'},
+    {name: 'lastModifiedDate', label: 'Last Modified Date'},
+  ];
+  isSelectAll = false;
+  selectedUsers = [];
   constructor(
     @Inject(NgZone) private zone: NgZone,
     private userService: UserService,
@@ -21,9 +30,36 @@ export class UserListComponent implements OnInit {
 
   ngOnInit() {
     this.userService.getUsers(null, null).subscribe( users => {
-      console.log(users);
+      // console.log(users);
       this.users = users;
     });
   }
 
+  toggleSelectAll() {
+    this.isSelectAll = !this.isSelectAll;
+    // console.log(this.isSelectAll);
+    if (this.isSelectAll) {
+      this.selectedUsers = this.users.map(user => {
+        return user.id;
+      });
+    } else {
+      this.selectedUsers = [];
+    }
+    // console.log(this.selectedUsers);
+  }
+
+  selectUser(e, id) {
+    // console.log(e.target.checked + ' : ' + id);
+    if (e.target.checked) {
+      this.selectedUsers.push(id);
+    } else {
+      const index = this.selectedUsers.indexOf(id);
+      this.selectedUsers.splice(index, 1);
+    }
+    // console.log(this.selectedUsers);
+  }
+
+  console(x) {
+    console.log(x);
+  }
 }
