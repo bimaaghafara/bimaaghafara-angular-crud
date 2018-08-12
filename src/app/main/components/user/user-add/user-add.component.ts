16c61c6e36a1d0aae254172feeca02c35e18e6d8
bimaaghafara <bimaa7@gmail.com>
2018-08-12 09:09:08 +0700

    add momentJs, lastModifiedBy, lastModifiedDate

--- src/app/main/components/user/user-add/user-add.component.ts
+++ src/app/main/components/user/user-add/user-add.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+// 3rd library
+import * as moment from 'moment';
+
 // service
+import { LocalStorageService } from 'angular-2-local-storage';
 import { LoginService } from '../../../services/login.service';
+import { UserService } from '../../../services/user.service';
+
+// model
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-user-add',
@@ -10,6 +18,7 @@ import { LoginService } from '../../../services/login.service';
   styleUrls: ['./user-add.component.scss']
 })
 export class UserAddComponent implements OnInit {
+  user = new User();
   model = {
     userName: '', firstName: '', lastName: '', active: false, password: '', confirmPassword: ''
   };
@@ -19,9 +28,13 @@ export class UserAddComponent implements OnInit {
   };
 
   isAllowSubmit = false;
+  isUserNameUsed = false;
 
   constructor(
+    private localStorageService: LocalStorageService,
     private loginService: LoginService,
+    private userService: UserService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -34,18 +47,45 @@ export class UserAddComponent implements OnInit {
     modelKeys.map(modelKey => {
       this.isValid[modelKey] = this.model[modelKey] ? true : false;
       this.isAllowSubmit = this.isAllowSubmit && this.model[modelKey];
+      if (modelKey === 'confirmPassword') {
+        this.isValid.confirmPassword = this.model.password === this.model.confirmPassword && !!this.model.confirmPassword ? true : false;
+        this.isAllowSubmit = this.isAllowSubmit && this.isValid.confirmPassword;
+      }
     });
     this.isAllowSubmit = this.isAllowSubmit ? true : false;
   }
 
+  doAddUser() {
+    const currentUser: User = this.localStorageService.get('user');
+    this.user.lastModifiedBy = `${currentUser.firstName} ${currentUser.lastName}`;
+    this.user.lastModifiedDate = moment().format('DD/MM/YYYY HH:mm');
+    this.user = Object.assign(this.user, this.model);
+    // console.log(this.user);
+    this.userService.addUser(this.user).subscribe( user => {
+      console.log('Add new user success!');
+      this.router.navigate(['/users']);
+    });
+  }
+
   onSubmit() {
+    console.clear();
     this.doValidation();
     console.log('Button add user was clicked!');
-    console.log(this.isAllowSubmit);
+    // console.log(this.isAllowSubmit);
+    // console.log(this.model);
+    if (this.isAllowSubmit) {
+      this.userService.getUserByUsername(this.model.userName).subscribe(user => {
+        if (user.length === 0 ) {
+          this.doAddUser();
+        } else {
+          this.isUserNameUsed = true;
+        }
+      });
+    }
   }
 
   onCancel() {
-    console.log(this.model);
+    this.router.navigate(['/users']);
   }
 
 }
--- src/app/main/services/user.service.ts
+++ src/app/main/services/user.service.ts
@@ -1,4 +1,4 @@
-import { Http, Response, URLSearchParams, RequestOptionsArgs } from '@angular/http';
+import { Http, Response, URLSearchParams, Headers, RequestOptionsArgs } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Observable } from '../../../../node_modules/rxjs';
@@ -10,10 +10,29 @@ import { User } from '../models/user';
 
 export class UserService {
     usersUrl = 'http://localhost:3000/users';
-
     constructor(
         private http: Http,
     ) { }
+
+    addUser(user): Observable<any> {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        const options: RequestOptionsArgs = {};
+        options.headers = headers;
+        return this.http.post(this.usersUrl, user, options).pipe(
+            map(res => res.json())
+        );
+    }
+
+    getUserByUsername(userName) {
+        const options: RequestOptionsArgs = {
+            params: {userName: userName}
+        };
+        const user = this.http.get(this.usersUrl, options).pipe(
+            map(res => res.json())
+        );
+        return user;
+    }
 
     getUsers(page, limit): Observable<User[]> {
         const options: RequestOptionsArgs = {
